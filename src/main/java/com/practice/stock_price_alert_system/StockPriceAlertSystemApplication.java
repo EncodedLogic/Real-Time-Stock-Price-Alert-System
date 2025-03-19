f41f8ba913e78191576fb3755e8e33cf98a66820
EncodedLogic <anandukumar052@gmail.com>
2025-03-20 01:53:41 +0530

    Initial commit

---
+++ src/main/java/com/practice/stock_price_alert_system/StockPriceAlertSystemApplication.java
@@ -0,0 +1,13 @@
+package com.practice.stock_price_alert_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceAlertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceAlertSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/stock_price_alert_system/StockPriceAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.stock_price_alert_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceAlertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
